55314ddf141c1dc5d5103dc72f05dd2c79991f36
Rafi Ramadhana <rf.ramadhana@gmail.com>
2020-11-01 10:29:07 +0700

    Implement data and methods

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ new Vue({
     // refer to element based on its id
     el:'#vue-app',
     data: {
-        name:'Shaun'
+        name:'Shaun',
+        job:'Ninja'
+    },
+    methods: {
+        greet:function(time){
+            // NOTE: 'this' referes to Vue object.
+            // NOTE: Vue take all the properties in data to the topper
+            // level.
+            return 'Good ' + time + ', ' + this.name +'!';
+        }
     }
 });
