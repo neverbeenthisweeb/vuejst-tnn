5669ad169450cb2da5f7d27c7202cd71bc0f3142
Rafi Ramadhana <rf.ramadhana@gmail.com>
2020-11-01 19:50:56 +0700

    Implement events

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ new Vue({
     // refer to element based on its id
     el:'#vue-app',
     data: {
-        name:'Shaun',
-        job:'Ninja',
-        website: 'https://www.google.com',
-        websiteTag:'<a href="https://www.google.com">Google</a>'
+        age:25,
+        x:0,
+        y:0
     },
     methods: {
-        greet:function(time){
-            // NOTE: 'this' referes to Vue object.
-            // NOTE: Vue take all the properties in data to the topper
-            // level.
-            return 'Good ' + time + ', ' + this.name +'!';
+        add:function(inc){
+            this.age += inc;
+        },
+        subtract:function(dec){
+            this.age -= dec;
+        },
+        updateXY:function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
